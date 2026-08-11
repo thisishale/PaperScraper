e07fd87d5fa40e2da7eae2e4c616373b4fec7027
Haleh Damirchi <haledamirchi@gmail.com>
2026-08-11 14:06:18 -0400

    update the args number

--- evaluate.py
+++ evaluate.py
@@ -167,7 +167,7 @@ def main():
     # predictions from (and writes eval_summary.json into) the same
     # results_{num_result}/ directory that run produced.
     flag_parser = argparse.ArgumentParser(add_help=False)
-    flag_parser.add_argument("--num-result", type=int, default=4)
+    flag_parser.add_argument("--num-result", type=int, default=5)
     num_result = flag_parser.parse_known_args()[0].num_result
     results_dir = f"results_{num_result}"
     eval_reports_dir = f"eval_reports_{num_result}"
